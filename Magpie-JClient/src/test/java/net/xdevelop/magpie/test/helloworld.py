4bd3c90209a9be9aca28056fe5d7dad3f881d4a0
John.Huang <john.h.cn@gmail.com>
2016-10-10 17:06:25 +0800

    Create project

---
+++ Magpie-JClient/src/test/java/net/xdevelop/magpie/test/helloworld.py
@@ -0,0 +1,3 @@
+a = 2
+b = 3
+_result_ = ["hello world", a, b, {"axb": a * b}]
---
+++ Magpie-PyServe/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Magpie',
+      version='0.1',
+      author='John Huang',
+      author_email='john.h.cn@gmail.com',
+      url='https://github.com/johnhuang-cn/Magpie',
+      package=['magpie'])
